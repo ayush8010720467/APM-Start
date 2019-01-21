dfed054efc1e993ef974390ec807d4767450daa3
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-01-21 14:45:36 +0530

    implementation of the services in angular 2

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -30,50 +30,9 @@ export class ProductListComponent implements OnInit{
     
 
     filteredProducts: IProduct[];
-    products: IProduct[]=[
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          },
-          {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-          },
-          {
-            "productId": 10,
-            "productName": "Video Game Controller",
-            "productCode": "GMG-0042",
-            "releaseDate": "October 15, 2015",
-            "description": "Standard two-button video game controller",
-            "price": 35.95,
-            "starRating": 4.6,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-          }
-    ];
-    constructor(){
-      this.filteredProducts=this.products;
+    products: IProduct[]=[];
+    constructor(private productService: ProductService){
+      
       this.listFilter = 'cart';
     }
     onRatingClicked(message: string): void{
@@ -88,7 +47,8 @@ export class ProductListComponent implements OnInit{
       this.showImage = !this.showImage;
     }
     ngOnInit():void{
-      console.log('in OnInit');
+      this.products = this.productService.getProducts();
+      this.filteredProducts=this.products;
     }
 
 }
