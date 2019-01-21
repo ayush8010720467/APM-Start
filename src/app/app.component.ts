a6426a4af9390f4f3d565cf299ebc517e045560f
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-01-21 18:03:27 +0530

    Routing Basic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,18 @@ import { Component } from '@angular/core';
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
   template:
-  `<div><h1>{{pageTitle}}</h1>
-   <pm-products></pm-products>
-    </div>`
+  `
+  <nav class = 'navbar navbar-expand navbar-light bg-light'>
+  <a class = 'navbar-brand'>{{pageTitle}}</a>
+  <ul class="nav nav-pills">
+  <li><a class = 'nav-link' [routerLink]="['/welcome']">Home</a></li>
+  <li><a class = 'nav-link' [routerLink]="['/products']">Product List</a></li>
+   </ul>
+ </nav>
+ <div class = 'container'>
+  <router-outlet></router-outlet>
+ </div>
+  `
 })
 export class AppComponent {
   //title = 'Angular: Getting Started';
